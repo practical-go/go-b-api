92c49f52a8ccea494c8a9389741227a7f5e8bd74
vibikerski <vibikerski@gmail.com>
2023-06-16 21:03:53 +0300

    MongoDB client added, handler tests modified

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -11,9 +13,18 @@ func main() {
 	var spfNewsClient SpaceflightClient
 	spfNewsClient.Init()
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var dbClient dbClient
+	dbClient.Init(ctx, "newsdb", "news")
+	defer func() { dbClient.Close(ctx) }()
+	// dbClient.upsortItem(ctx, "Elephant Yoga", "Houston Elephants Do Yoga 7-days a Week to Stay Flexible and Healthy")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, world!")
 	})
 	http.HandleFunc("/news", handleNews(&catFactsClient, &spfNewsClient))
 	http.ListenAndServe(":8080", nil)
+
 }
